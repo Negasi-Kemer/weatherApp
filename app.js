822f43fd13819fd15d23dbbf6c3fc82fbc0454a3
Negasi <negasi@Negasis-MacBook-Air.local>
2022-08-10 11:30:58 +0300

    Errors handled

--- app.js
+++ app.js
@@ -10,20 +10,39 @@ const url =
 
 // Mapbox url - geocode "Los Angeles"
 const geocode =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" +
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Addis%2Ababa.json?access_token=" +
   configs.mapbox.access_token +
   "&limit=1";
 
 // Send the geocode request
 request({ url: geocode, json: true }, (error, response) => {
-  const latitude = response.body.features[0].center[0];
-  const longitude = response.body.features[0].center[1];
-  console.log(latitude, longitude);
+  // Network error
+  if (error) {
+    console.log("Unable to connect to Mapbox services");
+  }
+  // Invalid token or input
+  else if (response.body.message) {
+    console.log(response.body.message);
+  }
+  // Address not found
+  else if (response.body.features.length == 0) {
+    console.log("Opps. No address found. Try with another one.");
+  } else {
+    const latitude = response.body.features[0].center[0];
+    const longitude = response.body.features[0].center[1];
+    console.log(latitude, longitude);
+  }
 });
 
 // Send request
 request({ url, json: true }, (error, response) => {
-  console.log(
-    `It's currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
-  );
+  if (error) {
+    console.log("Unable to connect to Weather services");
+  } else if (response.body.error) {
+    console.log(response.body.error.info);
+  } else {
+    console.log(
+      `It's currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+    );
+  }
 });
