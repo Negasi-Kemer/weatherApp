e3b7397e5683493cebebffab641bf02d154da388
Negasi <negasi@Negasis-MacBook-Air.local>
2022-08-10 06:45:23 +0300

    Log current weather

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const request = require("request");
 
 // Full url of the weather source
 const url =
-  "http://api.weatherstack.com/current?access_key=47306088c419b9d2886be432587ddd7f&query=37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=47306088c419b9d2886be432587ddd7f&query=37.8267,-122.4233?units=f";
 
 // Send request
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url, json: true }, (error, response) => {
+  console.log(
+    `It's currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`
+  );
 });
