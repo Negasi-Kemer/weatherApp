30d21eb3cf99f6db858fc2c22685cfd58931fd68
Negasi <negasi@Negasis-MacBook-Air.local>
2022-08-10 08:06:44 +0300

    Mapbox added for geocoding

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 // Require requesr module
 const request = require("request");
 
+// Require configs file
+const configs = require("./configs");
+
 // Full url of the weather source
 const url =
   "http://api.weatherstack.com/current?access_key=47306088c419b9d2886be432587ddd7f&query=37.8267,-122.4233?units=f";
+
+// Mapbox url - geocode "Los Angeles"
+const geocode =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" +
+  configs.mapbox.access_token +
+  "&limit=1";
+
+// Send the geocode request
+request({ url: geocode, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[0];
+  const longitude = response.body.features[0].center[1];
+  console.log(latitude, longitude);
+});
 
 // Send request
 request({ url, json: true }, (error, response) => {
---
+++ configs.js
@@ -0,0 +1,12 @@
+// Load environment varibales
+const dotenv = require("dotenv");
+
+// Add config.env file to the environmen varibales
+dotenv.config({ path: "config.env" });
+
+// Exportd variables
+module.exports = {
+  mapbox: {
+    access_token: process.env.MAP_BOX_TOKEN,
+  },
+};
